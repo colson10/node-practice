0e13e7771faa77f7d608497879aecca32a183bed
Carl Olson <colson10@gmail.com>
2018-04-11 10:53:25 -0700

    basic setup functioning

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var http = require('http');
+var url = require('url');
+
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log('Request for ' + pathname + ' received.');
+
+    route(handle, pathname);
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World2');
+    response.end();
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started');
+}
+
+exports.start = start;
